8ecb75fe047b5c2e6b5ad73256343dc72fcb2df3
brawndo <brandon.kmetz@gmail.com>
2025-11-03 15:56:04 -0800

    cruft kill

--- personal/brawndo.el
+++ personal/brawndo.el
@@ -212,71 +212,33 @@ Intended as an element of `compilation-finish-functions'."
 
   )
 
-;; (use-package lsp-sonarlint
-;;   :custom
-;;   ;; Allow sonarlint to download and unzip the official VSCode extension
-;;   ;; If nil, you'll have to do that yourself. See also `lsp-sonarlint-download'
-;;   ;; `lsp-sonarlint-download-url' and `lsp-sonarlint-download-dir'
-;;   (lsp-sonarlint-auto-download t)
+(use-package lsp-sonarlint
+  :custom
+  ;; Allow sonarlint to download and unzip the official VSCode extension
+  ;; If nil, you'll have to do that yourself. See also `lsp-sonarlint-download'
+  ;; `lsp-sonarlint-download-url' and `lsp-sonarlint-download-dir'
 
-;;   ;; Choose which analyzers you want enabled. By default all are enabled
-;;   ;; See command `lsp-sonarlint-available-analyzers' for the full list.
-;;   (lsp-sonarlint-enabled-analyzers '("java"
-;;                                      "cfamily"
-;;                                      "python"
-;;                                      "text")))
+  ;; (lsp-sonarlint-auto-download t)
+
+
+  ;;(lsp-sonarlint-use-system-jre t)
+  ;;(lsp-sonarlint-download-dir "/home/brawndo/Downloads/sonarlint-vscode-4.34.0/")
+
+
+  (lsp-sonarlint-download-dir "/home/brawndo/Downloads/sonarlint-vscode-linux-x64-4.34.0/")
+
+  ;; Choose which analyzers you want enabled. By default all are enabled
+  ;; See command `lsp-sonarlint-available-analyzers' for the full list.
+
+  ;; (lsp-sonarlint-enabled-analyzers '("java"
+  ;;                                    "cfamily"
+  ;;                                    "python"
+  ;;                                    "text"))
+
+)
 
 ;; NOTE(BK): install buildifer w/ go, put ~/go/bin on path and you're done
 (use-package bazel
   :config
   (setq bazel-buildifier-before-save t)
   (add-to-list 'auto-mode-alist '("\\.\\(BUILD\\)\\'" . bazel-mode)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Cruft
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (use-package simple-modeline
-;;   :hook (after-init . simple-modeline-mode))
-
-;; (use-package rmsbolt) ;; FIXME maybe need to change the bindings? it uses C-c C-c..
-
-(defun which-active-modes ()
-  "Give a message of which minor modes are enabled in the current buffer."
-  (interactive)
-  (let ((active-modes))
-    (mapc (lambda (mode) (condition-case nil
-                             (if (and (symbolp mode) (symbol-value mode))
-                                 (add-to-list 'active-modes mode))
-                           (error nil) ))
-          minor-mode-list)
-    (message "Active modes are %s" active-modes)))
-
-;; NOTE(BK): as of emacs 29.1, use-package is builtin!
-
-;; (unless (package-installed-p 'use-package)
-;;   (unless package-archive-contents
-;;     (package-refresh-contents))
-;;   (package-install 'use-package))
-
-;;(use-package vscode-dark-plus-theme)
-;;(use-package tron-legacy-theme)
-;;(use-package clues-theme)
-;;(use-package github-theme)
-;;(use-package darkokai-theme)
-;;(use-package spacemacs-theme)
-
-;; (use-package dashboard
-;;   :config
-;;   (dashboard-setup-startup-hook)
-;;   ;; Edits
-;;   (setq dashboard-banner-logo-title "Welcome, my master. They shall look upon our works and des
-;; pair!")
-;;   (setq dashboard-startup-banner 'logo)
-;;   (setq dashboard-items '((recents   . 5)
-;;                           (bookmarks . 5)
-;;                           (agenda    . 5))))
-
-;; XXX  https://github.com/hedronvision/bazel-compile-commands-extractor
-
-;; (setq warning-minimum-level :emergency)
