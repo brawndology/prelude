8a1294d09e696fa7a9f5d04077621d8432c08288
Brandon Kmetz <brandon.kmetz@gmail.com>
2026-06-30 10:38:36 -0700

    add (sorta) cmake-integration
    need to fix this at some point...

--- personal/brawndo.el
+++ personal/brawndo.el
@@ -295,6 +295,15 @@
 ;; TODO: experiment with this...
 ;; (use-package consult)
 
+
+;; TODO: screw around with this, if can't get it to work then i can just type
+;; presets by hand or have the build command save the preset...
+
+;; FIXME f 020 package or some shit not available...is this why it fails to see the cmake presets?
+;; (use-package cmake-integration
+;;   :vc (:url "https://github.com/darcamo/cmake-integration.git"
+;;             :rev :newest))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cruft
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
